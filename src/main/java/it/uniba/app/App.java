4cd833299c2edd46038ef02fdb3244e7831f9078
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 10:12:31 +0200

    aggiunta struttura iterativa oer l'inserimento ed il controllo del comando

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,17 +1,51 @@
 package it.uniba.app;
 
+import java.util.Scanner;
+
 /**
  * Main class of the application.
  */
 public final class App {
 
     public static void main(String[] args) {
-        if(args.length == 1 && (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("--help"))) {
+        if (args.length == 1 && (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("--help"))) {
             showHelp();
         }
+        BattleshipGame bg = new BattleshipGame();
+        Scanner s = new Scanner(System.in);
+        System.out.println("==== Battleship Game ====");
+        boolean exit = false;
+        do {
+            exit = false;
+            System.out.print("Inserisci un comando: ");
+            String command = s.nextLine();
+            switch (command.toLowerCase()){
+                case "/help":
+                    break;
+                case "/esci":
+                        break;
+                case "/facile":
+                    break;
+                case "/medio":
+                    break;
+                    case "/difficile":
+                        break;
+                case "/mostralivello":
+                    break;
+                case "/mostranavi":
+                    break;
+                case "/gioca":
+                    break;
+                case "/svelagriglia":
+                    break;
+                default:
+                    System.out.println("Comando non riconosciuto.");
+                    break;
+            }
+        } while (!exit);
     }
 
-    private static void showHelp(){
+    private static void showHelp() {
 
     }
 }
