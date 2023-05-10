3e4bf682657993dbd61d81057d0af7a7d0755dba
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 09:44:36 +0200

    aggiunti metodi mock della classe BattleshipGame

--- src/main/java/it/uniba/app/BattleshipGame.java
+++ src/main/java/it/uniba/app/BattleshipGame.java
@@ -1,5 +1,31 @@
 package it.uniba.app;
 
 public class BattleshipGame {
+    BattleshipGame() {
 
+    }
+
+    void showGrid() {
+
+    }
+
+    void newGame() {
+
+    }
+
+    void listShips() {
+
+    }
+
+    void showGameInfo() {
+
+    }
+
+    void setDiffiiculty() {
+
+    }
+
+    void closeGame() {
+
+    }
 }
