2c7eda7ff16991bdb269824891db148a6240d8ba
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 12:06:02 +0200

    risolto warning di checkstyle

--- src/main/java/it/uniba/app/ships/Orientation.java
+++ src/main/java/it/uniba/app/ships/Orientation.java
@@ -1,5 +1,9 @@
 package it.uniba.app.ships;
 
+/**
+ * Enumerazione che rappresenta l'orientamento di una nave.
+ * Può essere orizzontale o verticale.
+ */
 public enum Orientation {
     HORIZONTAL,
     VERTICAL
