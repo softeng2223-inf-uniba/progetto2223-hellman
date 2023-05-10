1501d1382c23b1bd6ee6afd5fbc53f967428bddb
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 11:47:10 +0200

    risolti warning di checkstyle

--- src/main/java/it/uniba/app/ships/Incrociatore.java
+++ src/main/java/it/uniba/app/ships/Incrociatore.java
@@ -2,8 +2,22 @@ package it.uniba.app.ships;
 
 import it.uniba.app.util.Pair;
 
-public class Incrociatore extends Ship {
-    public Incrociatore(Orientation orientation, Pair startingPosition) {
-        super(3, orientation, startingPosition);
+/**
+ * Classe che rappresenta l'incrociatore (nave da 3 caselle).
+ * Estende la classe astratta Ship.
+ *
+ * @see Ship
+ */
+public final class Incrociatore extends Ship {
+    private static final int LENGTH = 3;
+
+    /**
+     * Costruttore della classe Incrociatore.
+     *
+     * @param orientation      orientamento della nave
+     * @param startingPosition cella di partenza della nave
+     */
+    public Incrociatore(final Orientation orientation, final Pair startingPosition) {
+        super(LENGTH, orientation, startingPosition);
     }
 }
