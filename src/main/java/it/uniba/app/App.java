7f03e2c8e580f9ff91f05f50d308d7fa60c6c402
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 10:00:33 +0200

    Revert "creato metodo main"
    This reverts commit 089e2737

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,45 +1,11 @@
 package it.uniba.app;
 
-import java.util.Scanner;
-
 /**
  * Main class of the application.
  */
 public final class App {
 
     public static void main(String[] args) {
-        if (args.length == 1 && (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("--help")))
-            showHelp();
-        boolean exit = false;
-        do {
-            Scanner s = new Scanner(System.in);
-            System.out.println("==== Battleship Game ====");
-            System.out.print("Inserisci un comando >> ");
-            String command = s.nextLine();
-            if (!command.startsWith("/"))
-                System.out.println("Comando non valido!");
-            else switch (command.toLowerCase().replace("/", "")) {
-                case "esci":
-                    break;
-                case "facile":
-                    break;
-                case "medio":
-                    break;
-                case "difficile": 
-                    break;
-                case "mostralivello":
-                    break;
-                case "mostranavi":
-                    break;
-                case "gioca":
-                    break;
-                case "svelagriglia":
-                    break;
-            }
-        } while (!exit);
-    }
-
-    private static void showHelp() {
 
     }
 }
