6c79fc9184698e6e71c71a35a8e139f169d52545
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 11:50:26 +0200

    risolti warning di checkstyle

--- src/main/java/it/uniba/app/ships/Portaerei.java
+++ src/main/java/it/uniba/app/ships/Portaerei.java
@@ -2,8 +2,21 @@ package it.uniba.app.ships;
 
 import it.uniba.app.util.Pair;
 
+/**
+ * Classe che rappresenta la portaerei (nave da 5 caselle).
+ * Estende la classe astratta Ship.
+ *
+ * @see Ship
+ */
 public class Portaerei extends Ship {
-    public Portaerei(Orientation orientation, Pair startingPosition) {
-        super(5, orientation, startingPosition);
+    private static final int LENGTH = 5;
+    /**
+     * Costruttore della classe Portaerei.
+     *
+     * @param orientation      orientamento della nave
+     * @param startingPosition cella di partenza della nave
+     */
+    public Portaerei(final Orientation orientation, final Pair startingPosition) {
+        super(LENGTH, orientation, startingPosition);
     }
 }
