3954b2817fbc9a35850d3e4806bada79064bb00e
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 10:07:19 +0200

    Aggiunto handler per le flag -h e --help

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -6,6 +6,12 @@ package it.uniba.app;
 public final class App {
 
     public static void main(String[] args) {
+        if(args.length == 1 && (args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("--help"))) {
+            showHelp();
+        }
+    }
+
+    private static void showHelp(){
 
     }
 }
