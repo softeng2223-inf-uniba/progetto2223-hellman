defe8d4f9834275a7ddd2508c9d9cccc76935d05
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 11:49:27 +0200

    risolti warning di checkstyle

--- src/main/java/it/uniba/app/ships/Corazzata.java
+++ src/main/java/it/uniba/app/ships/Corazzata.java
@@ -2,8 +2,22 @@ package it.uniba.app.ships;
 
 import it.uniba.app.util.Pair;
 
-public class Corazzata extends Ship{
-    public Corazzata(Orientation orientation, Pair startingPosition) {
-        super(4, orientation, startingPosition);
+/**
+ * Classe che rappresenta la corazzata (nave da 4 caselle).
+ * Estende la classe astratta Ship.
+ *
+ * @see Ship
+ */
+public final class Corazzata extends Ship {
+    private static final int LENGTH = 4;
+
+    /**
+     * Costruttore della classe Corazzata.
+     *
+     * @param orientation      orientamento della nave
+     * @param startingPosition cella di partenza della nave
+     */
+    public Corazzata(final Orientation orientation, final Pair startingPosition) {
+        super(LENGTH, orientation, startingPosition);
     }
 }
