ffc05a2b5aab83af7e61f58064d701ac6cba1a9a
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 12:09:42 +0200

    risolti warning di checkstyle

--- src/main/java/it/uniba/app/ships/Cacciatorpediniere.java
+++ src/main/java/it/uniba/app/ships/Cacciatorpediniere.java
@@ -2,8 +2,21 @@ package it.uniba.app.ships;
 
 import it.uniba.app.util.Pair;
 
-public class Cacciatorpediniere extends Ship{
-    public Cacciatorpediniere(Orientation orientation, Pair startingPosition) {
-        super(2, orientation, startingPosition);
+/**
+ * Classe che rappresenta il cacciatorpediniere (nave da 2 caselle).
+ * Estende la classe astratta Ship.
+ *
+ * @see Ship
+ */
+public final class Cacciatorpediniere extends Ship {
+    private static final int LENGTH = 2;
+    /**
+     * Costruttore della classe Cacciatorpediniere.
+     *
+     * @param orientation      orientamento della nave
+     * @param startingPosition cella di partenza della nave
+     */
+    public Cacciatorpediniere(final Orientation orientation, final Pair startingPosition) {
+        super(LENGTH, orientation, startingPosition);
     }
 }
