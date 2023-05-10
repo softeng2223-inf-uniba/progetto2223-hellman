d458c814cf360f56345d8690415e8f70f631a2c4
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-10 11:38:35 +0200

    risolti warning di checkstyle

--- src/main/java/it/uniba/app/util/Pair.java
+++ src/main/java/it/uniba/app/util/Pair.java
@@ -1,26 +1,47 @@
 package it.uniba.app.util;
 
 /**
- * Classe che modella una coppia carattere-intero per rappresentare le coordinate di una cella della griglia.
- * La coppia è immutabile e per istanziarla si usa il metodo statico createPair della classe Builder.
- * @see Builder
+ * Classe che modella una coppia carattere-intero per rappresentare
+ * le coordinate di una cella della griglia.
+ * La coppia è immutabile e per istanziarla si usa il metodo statico
+ * createPair della classe Builder.
  */
-public class Pair {
+public final class Pair {
     private final Character first;
     private final Integer second;
+    private static final int MIN_INT = 1;
+    private static final int MAX_INT = 10;
+    private static final String ERR_MSG =
+            "Il secondo valore deve essere compreso tra 1 e 10";
 
     /**
-     * Costruttore della coppia
-     * @param first il carattere
-     * @param second l'intero
+     * Costruttore della coppia.
+     *
+     * @param firstItem  il carattere
+     * @param secondItem l'intero
+     * @throws IllegalArgumentException se il carattere
+     *                                  non è compreso tra A e J,
+     *                                  o se l'intero non è compreso tra 1 e 10
      */
-    private Pair(Character first, Integer second) {
-        this.first = first;
-        this.second = second;
+    public Pair(final Character firstItem, final Integer secondItem) throws IllegalArgumentException {
+        if (firstItem == 'A' || firstItem == 'B' || firstItem == 'C'
+                || firstItem == 'D' || firstItem == 'E' || firstItem == 'F'
+                || firstItem == 'G' || firstItem == 'H' || firstItem == 'I'
+                || firstItem == 'J') {
+            if (secondItem >= MIN_INT && secondItem <= MAX_INT) {
+                first = firstItem;
+                second = secondItem;
+            } else {
+                throw new IllegalArgumentException(ERR_MSG);
+            }
+        } else {
+            throw new IllegalArgumentException(ERR_MSG);
+        }
     }
 
     /**
-     * Restituisce il carattere della coppia
+     * Restituisce il carattere della coppia.
+     *
      * @return il carattere
      */
     public Character getFirst() {
@@ -28,7 +49,8 @@ public class Pair {
     }
 
     /**
-     * Restituisce l'intero della coppia
+     * Restituisce l'intero della coppia.
+     *
      * @return l'intero
      */
     public Integer getSecond() {
@@ -36,7 +58,10 @@ public class Pair {
     }
 
     /**
-     * Restituisce la coppia come array di interi. Il primo valore indica la posizione verticale, il secondo la posizione orizzontale.
+     * Restituisce la coppia come array di interi.
+     * Il primo valore indica la posizione verticale,
+     * il secondo la posizione orizzontale.
+     *
      * @return l'array degli indici
      */
     public int[] toArray() {
@@ -44,34 +69,12 @@ public class Pair {
     }
 
     /**
-     * Restituisce la coppia come stringa, nel formato "A-1"
+     * Restituisce la coppia come stringa, nel formato "A-1".
+     *
      * @return la stringa
      */
     @Override
     public String toString() {
         return first + "-" + second;
-    }
-
-    /**
-     * Classe utilitaria per la creazione di una coppia carattere-intero.
-     *
-     * @see Pair
-     */
-    class Builder {
-        /**
-         * Metodo statico per la creazione di una coppia carattere-intero.
-         * @param first il carattere
-         * @param second l'intero
-         * @return la coppia
-         * @throws IllegalArgumentException se il carattere non è compreso tra A e J o l'intero non è compreso tra 1 e 10
-         */
-        public static Pair createPair(char first, int second) throws IllegalArgumentException {
-            if (first == 'A' || first == 'B' || first == 'C' || first == 'D' || first == 'E' || first == 'F' || first == 'G' || first == 'H' || first == 'I' || first == 'J')
-                if (second >= 1 && second <= 10)
-                    return new Pair(first, second);
-                else
-                    throw new IllegalArgumentException("L'indice deve essere compreso tra 1 e 10 e la lettera tra A e J");
-            else throw new IllegalArgumentException("L'indice deve essere compreso tra 1 e 10 e la lettera tra A e J");
-        }
     }
 }
