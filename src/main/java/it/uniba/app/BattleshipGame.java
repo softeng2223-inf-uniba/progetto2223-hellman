97ffdbe1d008bcf82afc314f9216ff2fc572f2b9
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-16 10:39:02 +0200

    aggiunte precondizioni per la creazione della partita

--- src/main/java/it/uniba/app/BattleshipGame.java
+++ src/main/java/it/uniba/app/BattleshipGame.java
@@ -1,11 +1,20 @@
 package it.uniba.app;
 
+import it.uniba.app.exceptions.GameAlreadyRunningException;
+import it.uniba.app.exceptions.UnsetDifficultyException;
+import it.uniba.app.ships.Ship;
+
+import java.util.List;
+
 /**
  * Classe che rappresenta il gioco.
  */
 public final class BattleshipGame {
-    BattleshipGame() {
+    private Difficulty currentDifficulty;
+    private List<Ship> ships;
 
+    BattleshipGame() {
+        currentDifficulty = Difficulty.UNSET;
     }
 
     void setDifficulty() {
@@ -24,7 +33,12 @@ public final class BattleshipGame {
 
     }
 
-    void newGame() {
-
+    void newGame() throws UnsetDifficultyException, GameAlreadyRunningException {
+        if (currentDifficulty == Difficulty.UNSET) {
+            throw new UnsetDifficultyException("Devi impostare la difficoltà prima di iniziare una nuova partita.");
+        }
+        if (ships != null) {
+            throw new GameAlreadyRunningException("C'è già una partita in corso.");
+        }
     }
 }
