904c260d9b6c756478a3fa2ed87f00d8c1ef831a
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-05-16 10:58:23 +0200

    aggiunti campi relativi ai tentativi ed alla griglia

--- src/main/java/it/uniba/app/BattleshipGame.java
+++ src/main/java/it/uniba/app/BattleshipGame.java
@@ -10,8 +10,14 @@ import java.util.List;
  * Classe che rappresenta il gioco.
  */
 public final class BattleshipGame {
+    private static final int GRID_SIZE = 10;
     private Difficulty currentDifficulty;
     private List<Ship> ships;
+    private int maxFaliedAttempts;
+    private int failedAttempts;
+    private int hits;
+    private boolean[][] grid = new boolean[GRID_SIZE][GRID_SIZE];
+    private int[][] hitsGrid = new int[GRID_SIZE][GRID_SIZE];
 
     BattleshipGame() {
         currentDifficulty = Difficulty.UNSET;
