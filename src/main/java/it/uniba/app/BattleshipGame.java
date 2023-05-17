489a1bca6d5335d31988e4c75688ef93eee88da8
Eugenio Bursomanno <eugiga@live.it>
2023-05-17 17:13:55 +0200

    aggiunta documentazione

--- src/main/java/it/uniba/app/BattleshipGame.java
+++ src/main/java/it/uniba/app/BattleshipGame.java
@@ -71,6 +71,21 @@ public final class BattleshipGame {
 
     /**
      * Mostra la griglia dei colpi.
+     * La griglia sarà di questo tipo:
+     * <pre>
+     *     1  2  3  4  5  6  7  8  9  10
+     *   |------------------------------|
+     * A | 0  0  0  0  0  0  0  0  0  0 |
+     * B | 0  0  0  0  0  0  0  0  0  0 |
+     * C | 0  0  0  0  0  0  0  0  0  0 |
+     * D | 0  0  0  0  0  0  0  0  0  0 |
+     * E | 0  0  0  0  0  0  0  0  0  0 |
+     * F | 0  0  0  0  0  0  0  0  0  0 |
+     * G | 0  0  0  0  0  0  0  0  0  0 |
+     * H | 0  0  0  0  0  0  0  0  0  0 |
+     * I | 0  0  0  0  0  0  0  0  0  0 |
+     * J | 0  0  0  0  0  0  0  0  0  0 |
+     *   </pre>
      */
     void revealShipGrid() {
         String gridOutput = "   1  2  3  4  5  6  7  8  9  10\n";
