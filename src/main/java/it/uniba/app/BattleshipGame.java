7ed68c9a15c4ad386a6125bbddf051408528202d
vastrusa <v.agostinacchio1@studenti.uniba.it>
2023-05-17 18:10:56 +0200

    AGGIUNTO CODICE PER IL COMANDO /mostralivello
    Aggiunto codice al metodo showDifficulty(), in modo tale che controlla se il livello di difficoltà è impostato su Difficulty.UNSET e stampa un messaggio se non è stato impostato alcun livello di difficoltà. Altrimenti, stampa il livello di difficoltà impostato e il numero di tentativi falliti.
    Inoltre sono stati impostati i dati della classe e nella classe BattleshipGame() sono stati inizializzati.

--- src/main/java/it/uniba/app/BattleshipGame.java
+++ src/main/java/it/uniba/app/BattleshipGame.java
@@ -4,8 +4,13 @@ package it.uniba.app;
  * Classe che rappresenta il gioco.
  */
 public final class BattleshipGame {
-    BattleshipGame() {
+    private enum Difficulty { UNSET, EASY, MEDIUM, HARD }
+    private Difficulty difficulty;
+    private final int tentativiFalliti;
 
+    BattleshipGame() {
+        difficulty = Difficulty.UNSET;
+        tentativiFalliti = 0;
     }
 
     void setDifficulty() {
@@ -13,7 +18,12 @@ public final class BattleshipGame {
     }
 
     void showDifficulty() {
-
+        if (difficulty == Difficulty.UNSET) {
+            System.out.println("Non è stato impostato nessun livello di difficoltà");
+        } else {
+            System.out.println("Il livello di difficoltà impostato è : " + difficulty);
+            System.out.println("Il numero di tentativi falliti corrispondente è : " + tentativiFalliti);
+        }
     }
 
     void showShips() {
@@ -27,4 +37,5 @@ public final class BattleshipGame {
     void newGame() {
 
     }
+
 }
