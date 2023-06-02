1bbdeee76d719ccb5241e385cb208219dcb9e24b
Michele Angelini <micheleangelini@hotmail.com>
2023-06-02 16:08:45 +0200

    risolti errori segnalati da Checkstyle sulla clausola default

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -83,15 +83,15 @@ public final class App {
                     bg.revealShipGrid();
                     break;
                 default:
-                    if(command.startsWith("/facile")){
+                    if (command.startsWith("/facile")) {
                         final int facileLength = 8;
                         int customAttempts = Integer.parseInt(command.substring(facileLength));
                         bg.setCustomDifficulty("/facile", customAttempts);
-                    } else if (command.startsWith("/medio")){
+                    } else if (command.startsWith("/medio")) {
                         final int medioLength = 7;
                         int customAttempts = Integer.parseInt(command.substring(medioLength));
                         bg.setCustomDifficulty("/medio", customAttempts);
-                    } else if (command.startsWith("/difficile")){
+                    } else if (command.startsWith("/difficile")) {
                         final int difficileLength = 11;
                         int customAttempts = Integer.parseInt(command.substring(difficileLength));
                         bg.setCustomDifficulty("/difficile", customAttempts);
