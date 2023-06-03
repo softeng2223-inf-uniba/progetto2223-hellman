27c7a437f588ec6ce1a12ae345f016e7a4878519
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-06-03 15:22:35 +0200

    aggiunta chiamata al metodo

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -74,6 +74,7 @@ public final class App {
                     try {
                         bg.newGame();
                         System.out.println("Navi posizionate e partita iniziata.");
+                        bg.revealHitsGrid();
                     } catch (UnsetDifficultyException | GameAlreadyRunningException e) {
                         System.err.println(e.getMessage());
                     }
